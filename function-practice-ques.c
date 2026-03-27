e57149843889389c56fa929ed581c12d6e5f46d0
Manan Bansal <mananbansal583@gmail.com>
2026-03-27 15:43:47 +0530

    added a new program

--- function-practice-ques.c
+++ function-practice-ques.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <math.h>
 
-void sidesoftriangle(){
-  int a,b,c;
+void sidesoftriangle()
+{
+  int a, b, c;
   printf("Enter the sides of the traingle\n");
-  scanf("%d %d %d",&a,&b,&c);
-  float s,area;
-  if(a+b>c || a+c>b || b+c>a){
-    s=(a+b+c)/2;
-    area=sqrt(s*(s-a)*(s-b)*(s-c));
-    printf("Area of the traingle is %.2f\n",area);
+  scanf("%d %d %d", &a, &b, &c);
+  float s, area;
+  if (a + b > c || a + c > b || b + c > a)
+  {
+    s = (a + b + c) / 2;
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+    printf("Area of the traingle is %.2f\n", area);
   }
-  else{
+  else
+  {
     printf("Enter the valid sides of triangle\n");
   }
 }
 
-int main(){
-  sidesoftriangle();
+int factorial(int n)
+{
+  if (n == 0 || n == 1)
+  {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+int main()
+{
+  // sidesoftriangle();
+  int n;
+  printf("Enter the n\n");
+  scanf("%d", &n);
+  printf("%d\n", factorial(n));
 
   return 0;
 }
